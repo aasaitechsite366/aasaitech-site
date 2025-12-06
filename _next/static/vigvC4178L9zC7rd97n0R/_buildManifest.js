19fe2600de5e5f66641f1945e941b09f1b5c7049
sensordatascientist <datascimltrainee1@gmail.com>
2025-12-06 15:03:07 +0530

    Updates

--- _next/static/vigvC4178L9zC7rd97n0R/_buildManifest.js
+++ _next/static/vigvC4178L9zC7rd97n0R/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/.//_next/:path+",
-        "destination": "/aasaitech-site/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
